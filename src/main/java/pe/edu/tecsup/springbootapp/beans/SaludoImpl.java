3b6b11bec2d1448e6d1395179487f3ae512e4c1b
Jaime <you@example.com>
2021-11-23 21:08:17 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/springbootapp/beans/SaludoImpl.java
@@ -0,0 +1,23 @@
+package pe.edu.tecsup.springbootapp.beans;
+
+public class SaludoImpl implements Saludo {
+
+	@Override
+	public String dia(String nombres) {
+		// TODO Auto-generated method stub
+		return "Buenos días " + nombres;
+	}
+
+	@Override
+	public String tarde(String nombres) {
+		// TODO Auto-generated method stub
+		return "Buenas tardes " + nombres;
+	}
+
+	@Override
+	public String noche(String nombres) {
+		// TODO Auto-generated method stub
+		return "Buenas noches " + nombres;
+	}
+
+}
